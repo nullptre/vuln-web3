8b180decedca99048ab8cc862618518beb4f80a3
Yuri Nelipovich <ynelipovich@subject7.com>
2025-04-25 14:11:07 +0200

    stored xss with comments

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+comments = []  # In-memory storage for comments
 
 @app.route('/')
 def index():
@@ -10,6 +11,17 @@ def index():
 def search():
     query = request.args.get('search', '')
     return f"Search results for: {query}"
+
+@app.route('/comments')
+def show_comments():
+    return render_template('comments.html', comments=comments)
+
+@app.route('/comment', methods=['POST'])
+def add_comment():
+    comment = request.form.get('comment', '')
+    if comment:
+        comments.append(comment)
+    return redirect(url_for('show_comments'))
 
 if __name__ == '__main__':
     app.run(debug=True, port=8070) 
